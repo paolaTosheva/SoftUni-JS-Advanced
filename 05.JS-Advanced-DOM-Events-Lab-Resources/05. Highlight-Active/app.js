5c1ef719ac045c6f12a2ee8dbf46967df9033772
fearlessreality <pol1997@abv.bg>
2024-02-23 21:10:37 +0200

    Update app.js

--- 05.JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ 05.JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+  let inputs = document.querySelectorAll("div input[type='text']");
+  for (let input of inputs) {
+    input.addEventListener("focus", focus);
+    function focus() {
+      input.parentElement.classList.add("focused");
+    }
+
+    input.addEventListener("blur", function () {
+      input.parentElement.classList.remove("focused");
+      input.parentElement.style.backgroundColor = "";
+    });
+  }
 }
