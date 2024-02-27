2c81437b015cb91755d9df0ea55bfdfd1923244c
fearlessreality <pol1997@abv.bg>
2024-02-27 14:12:12 +0200

    Update app.js

--- 05.JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ 05.JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+  const input = document.getElementById("email");
+  const rightRegExp = new RegExp(/^([a-z]+)@([a-z]+)\.([a-z]+)/);
+  //<name>@<domain>.<extension>
+  //e.g. nakov@softuni.bg
+  input.addEventListener("change", function () {
+    if (!rightRegExp.test(input.value)) {
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
+
+  //class "error" if input = false
+  //if true ->clear the style
 }
