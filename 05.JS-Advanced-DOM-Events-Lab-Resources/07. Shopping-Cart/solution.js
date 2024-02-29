61bf58771a5029f0475f73f85da81c03b3ae9c7e
fearlessreality <pol1997@abv.bg>
2024-02-29 23:32:29 +0200

    Update solution.js

--- 05.JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ 05.JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+  let products = Array.from(document.querySelectorAll(".product"));
+  let totalPrice = 0;
+  let list = [];
+  products.forEach((product) => {
+    const name = product.querySelector(".product-title").textContent;
+    const price = Number(
+      product.querySelector(".product-line-price").textContent
+    );
+
+    Array.from(product.querySelectorAll(".add-product")).forEach((button) => {
+      button.addEventListener("click", function () {
+        document.querySelector(
+          "textarea"
+        ).value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+        totalPrice += price;
+        if (!list.includes(name)) {
+          list.push(name);
+        }
+      });
+    });
+  });
+
+  document.querySelector(".checkout").addEventListener("click", function () {
+    document.querySelector("textarea").value += `You bought ${list.join(
+      ", "
+    )} for ${totalPrice.toFixed(2)}.`;
+    Array.from(document.querySelectorAll(".add-product")).forEach(
+      (button) => (button.disabled = true)
+    );
+    document.querySelector(".checkout").disabled = true;
+  });
 }
