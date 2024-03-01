99b86d262d6b5d3dfd5d3cc36054b3b40d79c994
fearlessreality <pol1997@abv.bg>
2024-03-01 18:02:32 +0200

    Update 02. Add.js

---
+++ 06.JS-Advanced-Advanced-Functions-Lab/02. Add.js
@@ -0,0 +1,12 @@
+function solution(num1) {
+  return function (num2) {
+    return num1 + num2;
+  };
+}
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
