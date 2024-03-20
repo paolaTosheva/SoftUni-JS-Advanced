b98894bbcb333664d22fa55630329952f24ed7fd
fearlessreality <pol1997@abv.bg>
2024-03-20 17:51:28 +0200

    Update 06. List Processor.js

--- 06.JS-Advanced-Advanced-Functions-Lab/06. List Processor.js
+++ 06.JS-Advanced-Advanced-Functions-Lab/06. List Processor.js
@@ -1,24 +1,55 @@
-function processor(input) {
-  let innerCollection = {};
-  input.forEach((el) => {
-    let [command, string] = el.split(" ");
-    //   console.log(command);
-    //   console.log(string);
-    if (command === "add") {
-      console.log((innerCollection[string] = string));
-    }
-  });
-
-  console.log();
-  //   console.log(innerCollection);
-  //   return {
-  //     add() {},
-  //     remove() {},
-  //     print() {
-  //       console.log();
-  //     },
-  //   };
-}
+// function processor(input) {
+//   let innerCollection = [];
+//   input.forEach((el) => {
+//     let [command, string] = el.split(" ");
+//     // console.log(command);
+//     // console.log(string);
+//     if (command === "add") {
+//       add(string);
+//     } else if (command === "remove") {
+//       remove(string);
+//     } else if (command === "print") {
+//       print(innerCollection);
+//     }
+//   });
+//   function add(str) {
+//     innerCollection.push(str);
+//   }
+//   function remove(str) {
+//     innerCollection = innerCollection.filter((el) => el !== str);
+//   }
+//   function print(elements) {
+//     console.log(elements.join(","));
+//   }
+// }
 processor(["add hello", "add again", "remove hello", "add again", "print"]);
 console.log("-----------------");
 processor(["add pesho", "add george", "add peter", "remove peter", "print"]);
+//CORRECT
+//There's also another way:
+function createProcessor(input) {
+  let innerCollection = [];
+  input.forEach((element) => {
+    let [command, string] = element.split(" ");
+  });
+  return {
+    add() {},
+    remove() {},
+    print() {},
+  };
+}
+createProcessor([
+  "add hello",
+  "add again",
+  "remove hello",
+  "add again",
+  "print",
+]);
+console.log("-----------------");
+createProcessor([
+  "add pesho",
+  "add george",
+  "add peter",
+  "remove peter",
+  "print",
+]);
