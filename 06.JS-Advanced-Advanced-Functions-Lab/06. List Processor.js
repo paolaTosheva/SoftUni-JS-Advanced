82f66664585e545affd8aa830bc922729af04856
fearlessreality <pol1997@abv.bg>
2024-03-20 21:26:11 +0200

    Update 06. List Processor.js

--- 06.JS-Advanced-Advanced-Functions-Lab/06. List Processor.js
+++ 06.JS-Advanced-Advanced-Functions-Lab/06. List Processor.js
@@ -1,55 +1,29 @@
-// function processor(input) {
-//   let innerCollection = [];
-//   input.forEach((el) => {
-//     let [command, string] = el.split(" ");
-//     // console.log(command);
-//     // console.log(string);
-//     if (command === "add") {
-//       add(string);
-//     } else if (command === "remove") {
-//       remove(string);
-//     } else if (command === "print") {
-//       print(innerCollection);
-//     }
-//   });
-//   function add(str) {
-//     innerCollection.push(str);
-//   }
-//   function remove(str) {
-//     innerCollection = innerCollection.filter((el) => el !== str);
-//   }
-//   function print(elements) {
-//     console.log(elements.join(","));
-//   }
-// }
-// processor(["add hello", "add again", "remove hello", "add again", "print"]);
-// console.log("-----------------");
-// processor(["add pesho", "add george", "add peter", "remove peter", "print"]);
-//CORRECT
-//There's also another way:
-function createProcessor(input) {
+function processor(input) {
   let innerCollection = [];
-  input.forEach((element) => {
-    let [command, string] = element.split(" ");
+  input.forEach((el) => {
+    let [command, string] = el.split(" ");
+    // console.log(command);
+    // console.log(string);
+    if (command === "add") {
+      add(string);
+    } else if (command === "remove") {
+      remove(string);
+    } else if (command === "print") {
+      print(innerCollection);
+    }
   });
-  return {
-    add() {},
-    remove() {},
-    print() {},
-  };
+  function add(str) {
+    innerCollection.push(str);
+  }
+  function remove(str) {
+    innerCollection = innerCollection.filter((el) => el !== str);
+  }
+  function print(elements) {
+    console.log(elements.join(","));
+  }
 }
-createProcessor([
-  "add hello",
-  "add again",
-  "remove hello",
-  "add again",
-  "print",
-]);
+processor(["add hello", "add again", "remove hello", "add again", "print"]);
 console.log("-----------------");
-createProcessor([
-  "add pesho",
-  "add george",
-  "add peter",
-  "remove peter",
-  "print",
-]);
+processor(["add pesho", "add george", "add peter", "remove peter", "print"]);
+//CORRECT
+//NO OTHER WAY TO DO IT!!!
